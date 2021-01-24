e9a5f5ddf908fef121592c83be05d30009680dd2
Rohan Pethiyagoda <sarith21@gmail.com>
2021-01-24 10:49:12 +0000

    Add heap from git@github.com:RP-3/gheap.git

---
+++ heap/heap.go
@@ -0,0 +1,157 @@
+package heap
+
+const maxUint = ^uint(0)
+const maxInt = int(maxUint >> 1)
+
+// Orderable defines the properties that any item must have
+// to be heap-ordered.
+type Orderable interface {
+	// Order dictates the internal ordering of the items in the heap. Heap is
+	// min-ordered, so lowest-order items have the highest priority
+	Order() int
+}
+
+// Heap is a priority queue (min-heap)
+type Heap struct {
+	storage []Orderable
+	maxSize int
+}
+
+// NewHeap returns a Heap of the specified size. If size <= 0
+// heap size is unbounded.
+func NewHeap(maxSize int) *Heap {
+	if maxSize <= 0 {
+		return &Heap{maxSize: maxInt}
+	}
+	return &Heap{maxSize: maxSize}
+}
+
+// Heapify returns a Heap of the specified size using the given
+// source slice as its backing storage, and heap-sorts it in <= O(n) time.
+func Heapify(source []Orderable, maxSize int) *Heap {
+	result := &Heap{storage: source, maxSize: maxSize}
+	result.heapify()
+	return result
+}
+
+// Push adds an item to the heap.
+// The second return val, if true, indicates that the heap is at its
+// maximum capacity the highest priority item was popped and returned
+// to you as the first return val
+func (h *Heap) Push(val Orderable) (Orderable, bool) {
+	h.storage = append(h.storage, val)
+	h.percolateUp(len(h.storage) - 1)
+	if len(h.storage) > h.maxSize {
+		return h.Pop()
+	}
+	return nil, false
+}
+
+// UnsafeStorage yields a shallow copy of the underlying storage of the heap.
+// The behaviour following mutation of the copy or its pointers is undefined
+func (h *Heap) UnsafeStorage() []Orderable {
+	result := make([]Orderable, 0, len(h.storage))
+	copy(result, h.storage)
+	return result
+}
+
+// Pop removes the highest priority item from the heap.
+// The second return val, if false, indicates that the heap is empty
+// and that a nil value was returned to you as the first return val
+func (h *Heap) Pop() (Orderable, bool) {
+	switch len(h.storage) {
+	case 0:
+		return nil, false
+	case 1:
+		return h.removeLast(), true
+	default:
+		result := h.storage[0]
+		h.storage[0] = h.removeLast()
+		h.percolateDown(0)
+		return result, true
+	}
+}
+
+// Peak returns the highest priority item in the heap without
+// dequeuing it.
+// The second return val, if false, indicates that the heap is empty
+// and that a nil value was returned to you as the first return val
+func (h *Heap) Peak() (Orderable, bool) {
+	if len(h.storage) > 0 {
+		return h.storage[0], true
+	}
+	return nil, false
+}
+
+// Size returns the number of items in the Heap
+func (h *Heap) Size() int {
+	return len(h.storage)
+}
+
+func (h *Heap) removeLast() Orderable {
+	result := h.storage[len(h.storage)-1]
+	h.storage = h.storage[:len(h.storage)-1]
+	return result
+}
+
+func (h *Heap) percolateUp(i int) {
+	parentIndex := h.parentIndex(i)
+	for parentIndex >= 0 && parentIndex < i && !h.inOrder(parentIndex, i) {
+		h.storage[parentIndex], h.storage[i] = h.storage[i], h.storage[parentIndex]
+		i = parentIndex
+		parentIndex = h.parentIndex(i)
+	}
+}
+
+func (h *Heap) percolateDown(i int) {
+	childIndex := h.highestPriorityChildIndex(i)
+	for childIndex > -1 && !h.inOrder(i, childIndex) {
+		h.storage[i], h.storage[childIndex] = h.storage[childIndex], h.storage[i]
+		i = childIndex
+		childIndex = h.highestPriorityChildIndex(i)
+	}
+}
+
+// Returns the highest priority child index.
+// If there are no children, returns -1
+func (h *Heap) highestPriorityChildIndex(parentIndex int) int {
+	left, right := h.leftChildIndex(parentIndex), h.rightChildIndex(parentIndex)
+	switch {
+	case left >= len(h.storage):
+		return -1 // no children
+	case right >= len(h.storage):
+		return left // no right child
+	// both children exist
+	case h.storage[left].Order() <= h.storage[right].Order():
+		return left // left child greater or equal priority
+	default:
+		return right // right child greater priority
+	}
+}
+
+func (h *Heap) inOrder(parentIndex, childIndex int) bool {
+	return h.storage[parentIndex].Order() < h.storage[childIndex].Order()
+}
+
+func (h *Heap) parentIndex(childIndex int) int {
+	return (childIndex - 1) / 2
+}
+
+func (h *Heap) leftChildIndex(parentIndex int) int {
+	return parentIndex*2 + 1
+}
+
+func (h *Heap) rightChildIndex(parentIndex int) int {
+	return parentIndex*2 + 2
+}
+
+func (h *Heap) heapify() {
+	if len(h.storage) == 0 {
+		return
+	}
+	parentIndex := (len(h.storage) - 1) / 2 // skip the bottom row
+	for parentIndex >= 0 {
+		h.percolateDown(parentIndex)
+		parentIndex--
+	}
+}
---
+++ heap/heap_test.go
@@ -0,0 +1,316 @@
+package heap
+
+import (
+	"math"
+	"math/rand"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Heap", func() {
+	var subject *Heap
+
+	Describe("empty state inspection", func() {
+		BeforeEach(func() {
+			subject = NewHeap(-1)
+		})
+
+		Describe("peak", func() {
+			It("returns nil", func() {
+				_, exists := subject.Peak()
+				Expect(exists).To(Equal(false))
+			})
+		})
+
+		Describe("Size", func() {
+			It("returns 0", func() {
+				Expect(subject.Size()).To(Equal(0))
+			})
+		})
+	})
+
+	Context("when size is unbounded", func() {
+
+		Describe("Push", func() {
+			BeforeEach(func() {
+				subject = NewHeap(-1)
+			})
+
+			Context("when the heap is empty", func() {
+				item := testItem{1}
+				BeforeEach(func() {
+					Expect(subject.Size()).To(Equal(0))
+					subject.Push(item)
+				})
+
+				It("increases in size", func() {
+					Expect(subject.Size()).To(Equal(1))
+				})
+
+				It("places the new item at the head", func() {
+					obj, ok := subject.Peak()
+					Expect(ok).To(Equal(true))
+					Expect(equal(obj, item)).To(Equal(true))
+				})
+			})
+
+			Context("when the heap has a lower-priority item at the head", func() {
+				a, b := testItem{1}, testItem{2}
+				BeforeEach(func() {
+					subject.Push(a)
+					subject.Push(b)
+				})
+
+				It("Increases in size", func() {
+					Expect(subject.Size()).To(Equal(2))
+				})
+
+				It("does not replace the head item", func() {
+					item, ok := subject.Peak()
+					Expect(ok).To(Equal(true))
+					Expect(item.Order()).To(Equal(1))
+				})
+			})
+
+			Context("when the heap has a higher-priority item at the head", func() {
+				a, b := testItem{1}, testItem{2}
+				BeforeEach(func() {
+					subject.Push(b)
+					subject.Push(a)
+				})
+
+				It("Increases in size", func() {
+					Expect(subject.Size()).To(Equal(2))
+				})
+
+				It("does not replace the head item", func() {
+					item, ok := subject.Peak()
+					Expect(ok).To(Equal(true))
+					Expect(item.Order()).To(Equal(1))
+				})
+			})
+
+			Context("when the newest item requires just one swap", func() {
+				BeforeEach(func() {
+					subject.Push(testItem{4})
+					subject.Push(testItem{5})
+					subject.Push(testItem{8})
+					subject.Push(testItem{6})
+					subject.Push(testItem{9})
+					subject.Push(testItem{9})
+					subject.Push(testItem{7})
+				})
+
+				It("does not violate the heap ordering property", func() {
+					assertHeapOrdering(subject)
+				})
+			})
+		})
+
+		Describe("Pop", func() {
+			BeforeEach(func() {
+				subject = NewHeap(-1)
+			})
+
+			Context("when the heap is empty", func() {
+				It("returns nil", func() {
+					_, exists := subject.Pop()
+					Expect(exists).To(Equal(false))
+				})
+			})
+
+			Context("when the heap has a single item", func() {
+				item := testItem{1}
+				BeforeEach(func() {
+					subject.Push(item)
+				})
+
+				It("returns that item", func() {
+					obj, ok := subject.Pop()
+					Expect(ok).To(Equal(true))
+					Expect(equal(obj, item)).To(Equal(true))
+				})
+			})
+
+			Context("when the heap contains both higher and lower priority items", func() {
+				BeforeEach(func() {
+					subject.Push(testItem{key: 0})
+					subject.Push(testItem{key: 5})
+					subject.Push(testItem{key: 1})
+					subject.Push(testItem{key: 4})
+					subject.Push(testItem{key: 3})
+					Expect(subject.Size()).To(Equal(5))
+					subject.Push(testItem{key: 2}) // should sift to the middle
+					Expect(subject.Size()).To(Equal(6))
+				})
+
+				It("sorts items by their given order", func() {
+					lastVal := math.MinInt64
+					for subject.Size() > 0 {
+						assertHeapOrdering(subject)
+						top, ok := subject.Pop()
+						Expect(ok).To(Equal(true))
+						Expect(top.Order() > lastVal).To(Equal(true))
+						lastVal = top.Order()
+					}
+				})
+			})
+		})
+	})
+
+	Context("when a size is specified", func() {
+		heapSize := 5
+
+		Describe("Push", func() {
+			BeforeEach(func() {
+				subject = NewHeap(heapSize)
+			})
+
+			Context("when <= size items are inserted", func() {
+				BeforeEach(func() {
+					subject.Push(testItem{key: 1})
+					subject.Push(testItem{key: 5})
+					subject.Push(testItem{key: 2})
+					subject.Push(testItem{key: 4})
+					subject.Push(testItem{key: 3})
+				})
+
+				It("allows all items to exist inside", func() {
+					Expect(subject.Size()).To(Equal(heapSize))
+				})
+			})
+
+			Context("when additional items are inserted", func() {
+				BeforeEach(func() {
+					subject.Push(testItem{key: 0})
+					subject.Push(testItem{key: 5})
+					subject.Push(testItem{key: 1})
+					subject.Push(testItem{key: 4})
+					subject.Push(testItem{key: 3})
+					Expect(subject.Size()).To(Equal(heapSize))
+				})
+
+				It("does not exceed maximum size", func() {
+					subject.Push(testItem{key: 2})
+					Expect(subject.Size()).To(Equal(heapSize))
+				})
+
+				It("retains the lower-priority items", func() {
+					subject.Push(testItem{key: 2})
+					sortedContents := make([]int, 0, 5)
+					for subject.Size() > 0 {
+						assertHeapOrdering(subject)
+						item, ok := subject.Pop()
+						Expect(ok).To(Equal(true))
+						sortedContents = append(sortedContents, item.Order())
+					}
+					Expect(sortedContents).To(BeEquivalentTo([]int{1, 2, 3, 4, 5})) // zero is missing
+				})
+
+				It("ejects the highest-priority item", func() {
+					item, overflowed := subject.Push(testItem{key: 2})
+					Expect(overflowed).To(Equal(true))
+					Expect(item.Order()).To(Equal(0))
+				})
+			})
+		})
+	})
+
+	Describe("robustness", func() {
+
+		heapSize := -1 // unbounded
+		testSize := 200
+		popPercent := 25
+
+		Describe("heap ordering", func() {
+			BeforeEach(func() {
+				subject = NewHeap(heapSize)
+			})
+
+			It("never violates the heap ordering property", func() {
+				for i := 0; i < testSize; i++ {
+					if rand.Intn(100) > popPercent {
+						item := testItem{key: rand.Int()}
+						subject.Push(item)
+					} else {
+						subject.Pop()
+					}
+					assertHeapOrdering(subject)
+
+				}
+			})
+		})
+	})
+
+	Describe("heapify", func() {
+		Context("when the provided slice is empty", func() {
+			BeforeEach(func() {
+				subject = Heapify(make([]Orderable, 0), -1)
+			})
+
+			It("generates a valid (albeit empty) heap out of the given slice", func() {
+				assertHeapOrdering(subject)
+			})
+		})
+
+		Context("when the provided heap has items within it", func() {
+			BeforeEach(func() {
+				nums := []Orderable{
+					testItem{key: 1},
+					testItem{key: 9},
+					testItem{key: 2},
+					testItem{key: 8},
+					testItem{key: 3},
+					testItem{key: 7},
+					testItem{key: 4},
+					testItem{key: 6},
+					testItem{key: 5},
+					testItem{key: 4},
+					testItem{key: 6},
+					testItem{key: 3},
+					testItem{key: 7},
+					testItem{key: 2},
+					testItem{key: 8},
+					testItem{key: 1},
+					testItem{key: 9},
+				}
+				subject = Heapify(nums, -1)
+			})
+
+			It("generates a valid heap out of the given slice", func() {
+				assertHeapOrdering(subject)
+			})
+		})
+	})
+})
+
+// testing helpers
+type testItem struct {
+	key int
+}
+
+func (t testItem) Order() int {
+	return t.key
+}
+
+func equal(a Orderable, b testItem) bool {
+	obj, coerced := a.(testItem)
+	if !coerced {
+		return false
+	}
+	return obj.key == b.key
+}
+
+func assertHeapOrdering(heap *Heap) {
+	storage := heap.UnsafeStorage()
+	for i, item := range storage {
+		left, right := i*2+1, i*2+2
+		if left < len(storage) {
+			Expect(storage[left].Order() >= item.Order()).To(Equal(true))
+		}
+		if right < len(storage) {
+			Expect(storage[right].Order() >= item.Order()).To(Equal(true))
+		}
+	}
+}
